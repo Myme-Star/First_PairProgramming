9d1bb14715c021101116133afc40622e7e623dc4
g12912hm <g12912hm@s30424.tsuda.ac.jp>
2015-05-08 11:54:24 +0900

    ナビゲーター:sshino ドライバー:Myme-Star
    0から100までのテストケースを作成、成功するようにコーディング。

--- First_PairProgramming/src/intToEng/IntToEng.java
+++ First_PairProgramming/src/intToEng/IntToEng.java
@@ -16,7 +16,23 @@ public class IntToEng {
 
     // 数値を英訳する変換するメソッド
     static String translateEng(int n) {
-    	String u[] = 
-        return "";
+		String v[] = {"", "", "twenty", "thirty", "forty", "fifty", "sixty",
+				"seventy", "eighty", "ninety"};
+    	String u[] = {
+    			"zero", "one", "two", "three", "four", "five",
+    			"six", "seven", "eight", "nine", "ten", 
+    			"eleven", "twelve", "thirteen", "fourteen", "fifteen", 
+    			"sixteen", "seventeen", "eighteen", "nineteen", "twenty"};
+    	if(n<=20){
+    		return u[n];
+    	}else if(n<100){
+    		int i = n/10;
+    		int k = n%10;
+    		String ans = v[i];
+    		if(k!=0) ans += u[k];
+    		return ans;
+    	}else{
+    		return "one hundred";
+    	}
     }
 }
--- First_PairProgramming/test/intToEng/IntToEngTest.java
+++ First_PairProgramming/test/intToEng/IntToEngTest.java
@@ -10,12 +10,38 @@ public class IntToEngTest {
 	@Test
 	public void test() {
 		IntToEng ite = new IntToEng();
-		String s[] = {"zero","one", "two", "three", "four", "five"};
+		String s[] = {"zero","one", "two", "three", "four", "five",
+				"six", "seven", "eight", "nine", "ten", 
+    			"eleven", "twelve", "thirteen", "fourteen", "fifteen", 
+    			"sixteen", "seventeen", "eighteen", "nineteen", "twenty"};
 		for(int i=0; i<=5; i++){
 			String t = ite.translateEng(i);
 			String expected = s[i];
 			assertThat(t,is(expected));
 		}
+	}
+	
+	@Test
+	public void test2(){
+		IntToEng ite = new IntToEng();
+		String expected="";
+		String act = "";
+		String s1[] = {"", "", "twenty", "thirty", "forty", "fifty", "sixty",
+					"seventy", "eighty", "ninety"};
+		String s2[] = {"zero", "one", "two", "three", "four", "five",
+				"six", "seven", "eight", "nine"};
+		
+		for(int i=2; i<s1.length; i++){
+			for(int k=0; k<s2.length; k++){
+				expected = s1[i];
+				if(k>0) expected += s2[k];
+				act = ite.translateEng(i*10+k);
+				assertThat(act, is(expected));
+			}
+		}
+		expected = "one hundred";
+		act = ite.translateEng(100);
+		assertThat(act, is(expected));
 	}
 
 }
