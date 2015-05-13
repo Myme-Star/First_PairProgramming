32af62133533d2566ddc1fceaa43899eb9928f91
g12912hm <g12912hm@m20209.tsuda.ac.jp>
2015-05-13 12:27:51 +0900

    ドライバー：Myme-Star ナビゲーター：sshino
    プログラムのリファクタリング。
    一応99万くらいまでは対応できたはず。

--- First_PairProgramming/test/intToEng/IntToEngTest.java
+++ First_PairProgramming/test/intToEng/IntToEngTest.java
@@ -35,7 +35,7 @@ public class IntToEngTest {
 			for (int k = 0; k < s2.length; k++) {
 				expected = s1[i];
 				if (k > 0)
-					expected += s2[k];
+					expected += " "+s2[k];
 				act = ite.translateEng(i * 10 + k);
 				assertThat(act, is(expected));
 			}
@@ -66,8 +66,10 @@ public class IntToEngTest {
 					if (i > 0)
 						expected += s2[i] + " hundred ";
 
-					if (k >= 2)
+					if (k >= 2){
 						expected += s1[k];
+						if(l>0) expected += " ";
+					}
 
 					if (k * 10 + l < 20 && k * 10 + l >= 10) {
 						expected += s3[l + 1];
@@ -75,7 +77,7 @@ public class IntToEngTest {
 						if (expected.length() > 0 && l == 0)
 							;
 						else if (i > 0 && k == 0)
-							expected += " and " + s2[l];
+							expected += "and " + s2[l];
 						else
 							expected += s2[l];
 					}
@@ -90,11 +92,12 @@ public class IntToEngTest {
 	}
 
 	@Test
-	public void テスト4回目_10000まで() {
+	public void テスト4回目_9999まで() {
 		IntToEng ite = new IntToEng();
 		String expected = "";
 		int num = 0;
 		String act = "";
+
 		String s1[] = { "", "", "twenty", "thirty", "forty", "fifty", "sixty",
 				"seventy", "eighty", "ninety" };
 		String s2[] = { "zero", "one", "two", "three", "four", "five", "six",
@@ -102,9 +105,12 @@ public class IntToEngTest {
 		String s3[] = { "", "ten", "eleven", "twelve", "thirteen", "fourteen",
 				"fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
 
-		for (int h = 0; h <= 9; h++) {
-			for (int i = 0; i <= 9; i++) { // 100の位
-				for (int k = 0; k <= 9; k++) { // 10の位
+		for (int h = 0; h <= 9; h++)
+			// 1000の位
+			for (int i = 0; i <= 9; i++)
+				// 100の位
+				for (int k = 0; k <= 9; k++)
+					// 10の位
 					for (int l = 0; l <= 9; l++) { // 1の位
 						expected = "";
 						if (h > 0) {
@@ -115,8 +121,10 @@ public class IntToEngTest {
 						if (i > 0)
 							expected += s2[i] + " hundred ";
 
-						if (k >= 2)
+						if (k >= 2){
 							expected += s1[k];
+							if(l>0) expected += " ";
+						}
 
 						if (k * 10 + l < 20 && k * 10 + l >= 10) {
 							expected += s3[l + 1];
@@ -124,18 +132,26 @@ public class IntToEngTest {
 							if (expected.length() > 0 && l == 0)
 								;
 							else if (i > 0 && k == 0)
-								expected += " and " + s2[l];
+								expected += "and " + s2[l];
 							else
 								expected += s2[l];
 						}
-						num = h*1000 + i * 100 + k * 10 + l;
-						//System.out.println(h+" "+i+" "+k+" "+l+"   "+num);
+						num = h * 1000 + i * 100 + k * 10 + l;
+						// System.out.println(h+" "+i+" "+k+" "+l+"   "+num);
 						act = ite.translateEng(num);
 						assertThat(act, is(expected));
 
 					}
-				}
-			}
-		}
 	}
+
+	@Test
+	public void テストおまけ_一万以上を適当に() {
+		IntToEng ite = new IntToEng();
+		
+		String t = ite.translateEng(992301);
+		String expected = "nine hundred and ninety two thousand three hundred and one";
+		assertThat(t, is(expected));
+		
+	}
+
 }
